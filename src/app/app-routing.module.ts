b605701b93f045eaf89abcc865c2fb82fbd5ad65
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-30 15:27:20 -0500

    Rutas hijas creadas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'heroes',
+    loadChildren: () =>import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path: '404',
+    component: ErrorPageComponent
+  },
+  {
+    path: '**',
+    //component: ErrorPageComponent
+    redirectTo: '404'
+  }
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot (routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    children: [
+      {
+        path:'listado',
+        component: ListadoComponent
+      },
+      {
+        path:'agregar',
+        component: AgregarComponent
+      },
+      {
+        path:'editar:id',
+        component: AgregarComponent
+      },
+      {
+        path:'buscar',
+        component: BuscarComponent
+      },
+      {
+        path:':id',
+        component: HeroeComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'listado'
+      }
+    ]
+  }
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule { }
