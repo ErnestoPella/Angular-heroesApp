361c96d4e2fdd312cad89d97e832844777585455
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-30 21:04:21 -0500

    Pantalla de heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-heroe',
@@ -8,9 +11,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent implements OnInit{
 
-  constructor( private activatedRoute: ActivatedRoute){}
+  heroe!: Heroe;
+
+  constructor( private activatedRoute: ActivatedRoute, private heroesService: HeroesService){}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => console.log(id));
+    this.activatedRoute.params.pipe(
+      switchMap(({id}) => this.heroesService.getHeroePorId(id))
+    ).subscribe(heroe => this.heroe = heroe);
   }
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -14,4 +14,8 @@ export class HeroesService {
     return this.http.get<Heroe[]>('http://localhost:3000/heroes');
   }
 
+  getHeroePorId(id: string): Observable<Heroe>{
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+  }
+
 }
