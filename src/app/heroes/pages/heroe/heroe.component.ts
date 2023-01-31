7cba52e94587b2e66d4ce2a3ccfcce45b6e561a9
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-30 21:24:04 -0500

    Diseno de la pantalla de heroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from 'rxjs/operators';
@@ -13,11 +13,18 @@ export class HeroeComponent implements OnInit{
 
   heroe!: Heroe;
 
-  constructor( private activatedRoute: ActivatedRoute, private heroesService: HeroesService){}
+  constructor( private activatedRoute: ActivatedRoute,
+               private heroesService: HeroesService,
+               private router: Router){}
 
   ngOnInit(): void {
     this.activatedRoute.params.pipe(
       switchMap(({id}) => this.heroesService.getHeroePorId(id))
     ).subscribe(heroe => this.heroe = heroe);
+  }
+
+
+  regresar(){
+    this.router.navigate(['/heroes/listado']);
   }
 }
