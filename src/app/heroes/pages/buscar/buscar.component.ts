211f0e134d7f90b9f18053b6b5fbbe41ce933117
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-30 22:35:32 -0500

    Buscar Terminado

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -12,16 +12,23 @@ export class BuscarComponent {
 
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
+  
 
   constructor(private heroesService: HeroesService){}
   
   buscando(){
-    this.heroesService.getSugerencias(this.termino).subscribe( heroes => this.heroes = heroes);
+    this.heroesService.getSugerencias(this.termino.trim()).subscribe( heroes => this.heroes = heroes);
   }
 
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent){
+
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino =heroe.superhero;
 
