1cee92a902d577caeeffc180907e3acfb35b548d
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-01-31 17:45:54 -0500

    Diseno de la pantalla agregar heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregar.component.css']
 })
 export class AgregarComponent {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego:'',
+    characters:'',
+    first_appearance:'',
+    publisher: Publisher.DCComics,
+    alt_img:''
+
+  }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,6 +10,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [],
@@ -24,7 +25,8 @@ import {MatInputModule} from '@angular/material/input';
     MatGridListModule,
     MatAutocompleteModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
