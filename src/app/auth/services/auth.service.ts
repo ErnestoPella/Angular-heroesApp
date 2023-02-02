155586ed58edd8a8be195454760b17440deb10dd
ErnestoPella <104087343+ErnestoPella@users.noreply.github.com>
2023-02-01 21:11:02 -0500

    Mostrar información del usuario activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environment';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,19 @@ import { Auth } from '../interfaces/auth.interface';
 export class AuthService {
 
   private baseUrl: string = environments.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(){
+    return {...this._auth!};
+  }
 
   constructor( private http: HttpClient) { }
 
 
   login(){
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+    .pipe(
+      tap(auth => {this._auth = auth})
+    );
   }
 }
